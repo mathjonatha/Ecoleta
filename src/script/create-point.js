28a78a0d0a45711baf1c2a8981d38dbcff3c9f54
Heitor Augusto Lopes Nunes <heitoraugustoln@outlook.com>
2020-06-02 17:15:52 -0300

    Aula 2 Concluida

---
+++ src/script/create-point.js
@@ -0,0 +1,46 @@
+function populateStates() {
+  const stateSelect = document.querySelector("select[name=state]");
+
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((response) => {
+      return response.json();
+    })
+    .then((states) => {
+      for (let stateID = 0; stateID < states.length; stateID++) {
+        stateSelect.innerHTML += `<option value="${stateID + 1}">${
+          states[stateID].nome
+        }</option>`;
+      }
+    });
+}
+
+function getCities() {
+  const citySelect = document.querySelector("select[name=city]");
+  const stateInput = document.querySelector("[name=state]");
+
+  const stateValue = event.target.value;
+
+  const indexOfSelectedState = event.target.selectedIndex;
+  stateInput.value = event.target.options[indexOfSelectedState].text;
+
+  fetch(
+    `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateValue}/distritos`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((cities) => {
+      for (let cityID = 0; cityID < cities.length; cityID++) {
+        citySelect.innerHTML += `<option value="${cityID + 1}">${
+          cities[cityID].nome
+        }</option>`;
+      }
+      citySelect.disabled = false;
+    });
+}
+
+populateStates();
+
+document
+  .querySelector("select[name=state]")
+  .addEventListener("change", getCities);
